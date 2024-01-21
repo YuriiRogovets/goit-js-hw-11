9911d1ba50a0d0100c21a073785329216921f29a
YuriiRogovets <142502976+YuriiRogovets@users.noreply.github.com>
2024-01-21 17:11:27 +0000

    Deploying to gh-pages from @ YuriiRogovets/goit-js-hw-11@83efabb0f5e7d7c695d66a7a1ec52f2b7dc8ee1a 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,5 +1,5 @@
-import{i as u,S as d}from"./assets/vendor-46aac873.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function l(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=l(e);fetch(e.href,t)}})();const p=document.querySelector(".search-form"),f=document.querySelector(".gallery"),n=document.querySelector(".loader"),m="https://pixabay.com/api",y="41900218-778e908913d1efd90b8f97d56",h="photo",g="horizontal",v="true";n.style.display="none";p.addEventListener("submit",$);function $(s){s.preventDefault(),n.style.display="block";const r=s.currentTarget,l=r.elements.query.value;b(l).then(o=>{o.hits.length===0&&u.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",backgroundColor:"red",icon:"none"}),S(o.hits)}).catch(o=>console.error(o)).finally(()=>{r.reset(),n.style.display="none"})}function b(s){return fetch(`${m}/?key=${y}&q=${s}&image_type=${h}&orientation=${g}&safesearch=${v}`).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()})}const L=new d(".gallery a",{captionsData:"alt",captionDelay:250});function S(s){const r=s.map(({largeImageURL:l,webformatURL:o,tags:e,likes:t,views:i,comments:a,downloads:c})=>`<li class="gallery-card">
-        <a class="gallery-link" href="${o}">
+import{S as d,i as p}from"./assets/vendor-46aac873.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function l(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=l(e);fetch(e.href,t)}})();const f=document.querySelector(".search-form"),a=document.querySelector(".gallery"),n=document.querySelector(".loader"),m=new d(".gallery a",{captionsData:"alt",captionDelay:250});f.addEventListener("submit",h);function h(o){o.preventDefault(),n.style.display="block",a.innerHTML="";const s=o.currentTarget,l=s.elements.query.value;y(l).then(r=>{r.hits.length===0&&p.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",backgroundColor:"red",icon:"none"}),g(r.hits)}).catch(r=>console.error(r)).finally(()=>{s.reset(),n.style.display="none"})}function y(o){const s="https://pixabay.com/api",l="41900218-778e908913d1efd90b8f97d56",r=new URLSearchParams({key:l,q:o,image_type:"photo",orientation:"horizontal",safesearch:"true"});return fetch(`${s}/?${r}`).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()})}function g(o){const s=o.map(({largeImageURL:l,webformatURL:r,tags:e,likes:t,views:i,comments:c,downloads:u})=>`<li class="gallery-card">
+        <a class="gallery-link" href="${r}">
             <img 
                 class="gallery-image"
                     src="${l}"
@@ -17,12 +17,12 @@ import{i as u,S as d}from"./assets/vendor-46aac873.js";(function(){const r=docum
             </div>
             <div class="title-element">
                 <p class="title-text">Comments:</p>
-                <p class="title-value">${a} </p>
+                <p class="title-value">${c} </p>
             </div>
             <div class="title-element">
                 <p class="title-text">Downloads:</p>
-                <p class="title-value">${c} </p>
+                <p class="title-value">${u} </p>
             </div>
         </div>
-    </li>`).join("");f.innerHTML=r,L.refresh()}
+    </li>`).join("");a.innerHTML=s,m.refresh()}
 //# sourceMappingURL=commonHelpers.js.map
