d76e73bcfe4e3b076287ad78b017d26e21b2ea6d
YuriiRogovets <142502976+YuriiRogovets@users.noreply.github.com>
2024-01-23 16:35:53 +0000

    Deploying to gh-pages from @ YuriiRogovets/goit-js-hw-11@01b899a17779eeec5c53a1d1f4ecbfd5fe1c3d21 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,8 +1,8 @@
-import{S as d,i as f}from"./assets/vendor-46aac873.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function l(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=l(e);fetch(e.href,t)}})();const p=document.querySelector(".search-form"),a=document.querySelector(".gallery"),n=document.querySelector(".loader"),m=new d(".gallery a",{captionsData:"alt",captionDelay:250});p.addEventListener("submit",h);function h(o){o.preventDefault(),n.style.display="block",a.innerHTML="";const s=o.currentTarget,l=s.elements.query.value.trim();y(l).then(r=>{r.hits.length===0&&f.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",backgroundColor:"red",icon:"none"}),g(r.hits)}).catch(r=>console.error(r)).finally(()=>{s.reset(),n.style.display="none"})}function y(o){const s="https://pixabay.com/api",l="41900218-778e908913d1efd90b8f97d56",r=new URLSearchParams({key:l,q:o,image_type:"photo",orientation:"horizontal",safesearch:"true"});return fetch(`${s}/?${r}`).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()})}function g(o){const s=o.map(({largeImageURL:l,webformatURL:r,tags:e,likes:t,views:i,comments:c,downloads:u})=>`<li class="gallery-card">
+import{S as p,i as n}from"./assets/vendor-46aac873.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function l(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=l(e);fetch(e.href,t)}})();const f=document.querySelector(".search-form"),c=document.querySelector(".gallery"),a=document.querySelector(".loader"),m=new p(".gallery a",{captionsData:"alt",captionDelay:250});f.addEventListener("submit",h);function h(s){s.preventDefault(),a.style.display="block",c.innerHTML="";const r=s.currentTarget,l=r.elements.query.value.trim();y(l).then(o=>{o.hits.length===0&&n.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",backgroundColor:"red",icon:"none"}),g(o.hits)}).catch(o=>{n.error({message:"Oops, server connection error!",position:"topRight",backgroundColor:"red",icon:"none"})}).finally(()=>{r.reset(),a.style.display="none"})}function y(s){const r="https://pixabay.com/api",l="41900218-778e908913d1efd90b8f97d56",o=new URLSearchParams({key:l,q:s,image_type:"photo",orientation:"horizontal",safesearch:"true"});return fetch(`${r}/?${o}`).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()})}function g(s){const r=s.map(({largeImageURL:l,webformatURL:o,tags:e,likes:t,views:i,comments:u,downloads:d})=>`<li class="gallery-card">
         <a class="gallery-link" href="${l}">
             <img 
                 class="gallery-image"
-                    src="${r}"
+                    src="${o}"
                     alt="${e}"/>
         </a>
         
@@ -17,12 +17,12 @@ import{S as d,i as f}from"./assets/vendor-46aac873.js";(function(){const s=docum
             </div>
             <div class="title-element">
                 <p class="title-text">Comments:</p>
-                <p class="title-value">${c} </p>
+                <p class="title-value">${u} </p>
             </div>
             <div class="title-element">
                 <p class="title-text">Downloads:</p>
-                <p class="title-value">${u} </p>
+                <p class="title-value">${d} </p>
             </div>
         </div>
-    </li>`).join("");a.insertAdjacentHTML("beforeend",s),m.refresh()}
+    </li>`).join("");c.insertAdjacentHTML("beforeend",r),m.refresh()}
 //# sourceMappingURL=commonHelpers.js.map
